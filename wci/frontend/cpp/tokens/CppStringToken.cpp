32ecc5606d53991ce93393a4d5cced77d1751766
samglau4 <45607428+samglau4@users.noreply.github.com>
2019-02-15 19:57:57 -0800

    Update CppStringToken.cpp Only missing "\""

--- wci/frontend/cpp/tokens/CppStringToken.cpp
+++ wci/frontend/cpp/tokens/CppStringToken.cpp
@@ -11,7 +11,7 @@
 #include <string>
 #include "../CppError.h"
 
-#include "../CppError.h"
+#include "../../Cpp/CppError.h"
 
 namespace wci { namespace frontend { namespace cpp { namespace tokens {
 
@@ -30,7 +30,7 @@ void CppStringToken::extract() throw (string)
     string value_str = "";
 
     char current_ch = next_char();  // consume initial quote
-    text += '\"';
+    text += '"';
 
     // Get string characters.
     do
@@ -38,7 +38,7 @@ void CppStringToken::extract() throw (string)
         // Replace any whitespace character with a blank.
         if (isspace(current_ch)) current_ch = ' ';
 
-        if ((current_ch != '\"') && (current_ch != EOF))
+        if ((current_ch != '"') && (current_ch != EOF))
         {
             text += current_ch;
             value_str  += current_ch;
@@ -46,57 +46,54 @@ void CppStringToken::extract() throw (string)
         }
 
         // Quote?  Each pair of adjacent quotes represents a single-quote.
-        if (current_ch == '\"')
+        if (current_ch == '"')
         {
-            while ((current_ch == '\"') && (peek_char() == '\"'))
+            while ((current_ch == '"') && (peek_char() == '"'))
             {
-                text += '\"';
+                text += '"';
                 value_str  += current_ch;  // append single-quote
                 current_ch = next_char();  // consume pair of quotes
-//                current_ch = next_char();
             }
         }
-        if(current_ch == '\\')
-        {
-        	current_ch = next_char();
 
-        	if(current_ch == 't')
-        	{
-        		text += "\\";
-        		text += current_ch;
-        		value_str += "\t";
-        		current_ch = next_char();
-        	}
-        	else if(current_ch == 'n')
-        	{
-        		text += "\\";
-        		text += current_ch;
-        		value_str += "\n";
-        		current_ch = next_char();
-//        		current_ch = next_char();
-        	}
-        	else if(current_ch == '\"')
-        	{
-        		while ((current_ch == '\"') && (peek_char() == '\"'))
-        		{
-        			text += "\\";
-        			text += "\"";
-        			value_str += current_ch;
-        			current_ch = next_char();
-        			current_ch = next_char();
-        		}
-//        		text += '\"';
-//        		value_str += current_ch;
-        	}
-        }
-    } while ((current_ch != '\"') && (current_ch != Source::END_OF_FILE));
+        if((current_ch == '\\') && (peek_char() == 't'))
+		{
+			value_str += '\t';
+			text += "\\";
+			text += "t";
+			current_ch = next_char();
+			current_ch = next_char();
+		}
+		if((current_ch == '\\') && (peek_char() == 'n'))
+		{
+			value_str += '\n';
+			text += "\\";
+			text += "n";
+			current_ch = next_char();
+			current_ch = next_char();
+		}
 
-    if (current_ch == '\"')
+		if ((current_ch =='\\') && (peek_char() == '"'))
+        	{
+				value_str += "\"";
+				text += "\\";
+				text += "\"";
+        		current_ch = next_char();
+        		current_ch = next_char();
+
+        	}
+
+
+    } while ((current_ch != '"') && (current_ch != Source::END_OF_FILE));
+
+
+    if (current_ch == '"')
     {
-        next_char();  // consume final quote
-        text += '\"';
-        type = (TokenType) CppT_STRING;
-        value = value_str;
+        current_ch = next_char();  // consume final quote
+		text += '"';
+		type = (TokenType) CppT_STRING;
+		value = value_str;
+
     }
     else
     {
@@ -106,77 +103,3 @@ void CppStringToken::extract() throw (string)
 }
 
 }}}}  // namespace wci::frontend::pascal::tokens
-
-
-
-//void CppStringToken::extract() throw (string)
-//{
-//    string value_str = "";
-//
-//    char current_ch = next_char();  // consume initial quote
-//    text += "\"";
-//
-//    // Get string characters.
-//    do
-//    {
-//        // Replace any whitespace character with a blank.
-//        if (isspace(current_ch)) current_ch = ' ';
-//
-//        if ((current_ch != '"') && (current_ch != EOF))
-//        {
-//          	if(current_ch == '\\'){
-//            		text  += current_ch;
-//            		current_ch = next_char();
-//            		text  += current_ch;
-//            		if(current_ch == 't'){
-//            			value_str += '\t';
-//            			current_ch = next_char();
-//            		}
-//            		else if(current_ch == 'n'){
-//            			value_str += '\n';
-//            			current_ch = next_char();
-//            		}
-//            		else if(current_ch == '"'){
-//            			value_str += '"';
-//            			current_ch = next_char();
-//            		}
-//            		else{
-//            			value_str += '\\';
-//            			value_str += current_ch;
-//            			current_ch = next_char();
-//            		}
-//        	}
-//        	else{
-//				text += current_ch;
-//				value_str  += current_ch;
-//				current_ch = next_char();  // consume character
-//        	}
-//        }
-//
-//        // Quote?  Each pair of adjacent quotes represents a single-quote.
-//        if (current_ch == '\"')
-//        {
-//            while ((current_ch == '\"') && (peek_char() == '\"'))
-//            {
-//                text += "\"\"";
-//                value_str  += current_ch;  // append single-quote
-//                current_ch = next_char();  // consume pair of quotes
-//                current_ch = next_char();
-//            }
-//        }
-//    } while ((current_ch != '\"') && (current_ch != Source::END_OF_FILE));
-//
-//    if (current_ch == '\"')
-//    {
-//        next_char();  // consume final quote
-//        text += '\"';
-//        type = (TokenType) CppT_STRING;
-//        value = value_str;
-//    }
-//    else
-//    {
-//        type = (TokenType) CppT_ERROR;
-//        value = (int) UNEXPECTED_EOF;
-//    }
-//}
-//}}}}  // namespace wci::frontend::pascal::tokens
